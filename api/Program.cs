5bf70a59fc71c2c7d0513e94a67ffc595259b1b4
Henrik Binggl <henrik@binggl.net>
2018-06-28 21:29:57 +0200

    Add dinosaurs and Serilog.

--- api/Program.cs
+++ api/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Api
 {
@@ -14,6 +16,14 @@ namespace Api
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("Starting web host");
             CreateWebHostBuilder(args).Build().Run();
         }
 
@@ -35,6 +45,7 @@ namespace Api
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseConfiguration(configuration)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 ;
         }
     }
