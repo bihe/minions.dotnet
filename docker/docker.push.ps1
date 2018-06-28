5d8116d99502365b05382dd84d3706abd41e6942
Henrik Binggl <henrik@binggl.net>
2018-06-28 18:50:37 +0200

    Push the docker image.

--- docker/docker.push.ps1
+++ docker/docker.push.ps1
@@ -1,2 +1,4 @@
-docker tag minions $env:DOCKER_ID_USER/minions
-docker push $env:DOCKER_ID_USER/minions
+. .\_environment.ps1
+
+docker tag minions $env:DOCKER_ID_USER/minions-dotnet
+docker push $env:DOCKER_ID_USER/minions-dotnet
